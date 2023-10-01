0ee9a51251cd176d7e3aef7e91b0dfb3d0ab4fb2
Julie Rindone <julie.rindone@gmail.com>
2023-10-01 14:51:36 -0500

    Created files, started html & added event listener for button, added css

---
+++ index.js
@@ -0,0 +1,7 @@
+const saveInput = document.getElementById("input-btn")
+const inputEl = document.getElementById("input-el")
+let myLeads = []
+
+saveInput.addEventListener("click", function () {
+  console.log("button-clicked from event listener!")
+})
