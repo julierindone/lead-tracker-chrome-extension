930202d52c72dfc1d99898145439b67a778111ef
Julie Rindone <julie.rindone@gmail.com>
2023-10-07 09:25:32 -0500

    Created leads list

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-const saveInput = document.getElementById("input-btn")
-const inputEl = document.getElementById("input-el")
-let myLeads = []
+//// resource: 
+// https://www.geeksforgeeks.org/how-to-creating-html-list-from-javascript-array/
 
-saveInput.addEventListener("click", function () {
-  console.log("button-clicked from event listener!")
+const inputBtn = document.getElementById("input-btn")
+const inputEl = document.getElementById("input-el")
+//// let data 
+let myLeads = []
+//// let list...  
+const ulEl = document.getElementById("list-of-leads")
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value)
+
+  let li = document.createElement('li')
+  for (let i = 0; i < myLeads.length; i++) {
+    li.innerText = myLeads[i]
+  }
+  ulEl.appendChild(li)
 })
+
+
+// Scrimba solution for printing list to dom, which is shorter than what I came up with:
+// for (let i = 0; i < myLeads.length; i++) {
+//   ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+// }
