966f950a345716fcb0667aee3ff9d9e995330faa
Julie Rindone <julie.rindone@gmail.com>
2023-10-07 20:57:42 -0500

    Added local storage and delete function

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
 //// resource: 
 // https://www.geeksforgeeks.org/how-to-creating-html-list-from-javascript-array/
 
-const inputBtn = document.getElementById("input-btn")
-const inputEl = document.getElementById("input-el")
-//// let data 
 let myLeads = []
-//// let list...  
+const inputBtn = document.getElementById("input-btn")
+const deleteBtn = document.getElementById("delete-btn")
+const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("list-of-leads")
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage
+  renderLeads()
+}
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value)
-
-  let li = document.createElement('li')
-  for (let i = 0; i < myLeads.length; i++) {
-    li.innerText = myLeads[i]
-  }
-  ulEl.appendChild(li)
+  inputEl.value = ""
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
+  renderLeads()
 })
 
+function renderLeads() {
+  let listItems = ""
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems +=
+      `<li>
+        <a href="${myLeads[i]}" target="_blank">
+          ${myLeads[i]}
+        </a>
+      </li>`
+  }
+  ulEl.innerHTML = listItems
+}
 
-// Scrimba solution for printing list to dom, which is shorter than what I came up with:
-// for (let i = 0; i < myLeads.length; i++) {
-//   ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
-// }
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear()
+  myLeads = []
+  renderLeads()
+})
